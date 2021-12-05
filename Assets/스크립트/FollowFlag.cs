248d7173e7eabf82a466e4028ae51861862813ae
JiWonKimP <86395330+JiWonKimP@users.noreply.github.com>
2021-12-05 17:30:54 +0900

    flag

---
+++ Assets/스크립트/FollowFlag.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowFlag : MonoBehaviour
+{
+    public Transform Player;
+    public float hight = 5.0f;
+    public float dampTrace = 20.0f;
+
+    private Transform flagtr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        flagtr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    private void LateUpdate()
+    {
+        if(Player == null)
+        {
+            return;
+        }
+
+        flagtr.position = Vector3.Lerp(flagtr.position, Player.position + (Vector3.up * hight), Time.deltaTime * dampTrace);
+    }
+}
