5cb38b6cca53a6411331f83402f4108dd3f1a307
tjdqls1258 <42854743+tjdqls1258@users.noreply.github.com>
2021-12-17 22:22:43 +0900

    8
    8

--- Assets/스크립트/FlagCatch.cs
+++ Assets/스크립트/FlagCatch.cs
@@ -59,6 +59,7 @@ public class FlagCatch : MonoBehaviourPun, IPunObservable
         if (PhotonNetwork.IsMasterClient)
         {
             FollowPlayer_Name = PlayerName;
+            photonView.RPC("RPC_Get_Flag", RpcTarget.Others, FollowPlayer_Name);
             photonView.RPC("Rpc_Set_Target", RpcTarget.Others, FollowPlayer_Name);
         }
         Iscatched = true;
@@ -86,8 +87,8 @@ public class FlagCatch : MonoBehaviourPun, IPunObservable
         Flag.GetComponent<CapsuleCollider>().enabled = true;
         FollowPlayer_Name = null;
         Iscatched = false;
+        photonView.RPC("RPC_Drop_Flag", RpcTarget.Others, FollowPlayer_Name);
         photonView.RPC("Rpc_Set_Target", RpcTarget.Others, FollowPlayer_Name);
-        
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
